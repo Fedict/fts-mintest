d5ad9fefafd36e480d5670e0d86ac13ea8fd29be
chmo <chmo>
2024-04-02 18:09:34 +0200

    Add https proxy

--- test_fps/run.sh
+++ test_fps/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp "target/lib/*":target/test_fps-0.0.6-SNAPSHOT.jar com.bosa.testfps.Main -Dproxy.https.enabled=${PROXY_ENABLED:-true} -Dproxy.https.user=$PROXY_USER -Dproxy.https.password=$PROXY_PASSWORD -Dproxy.https.host=$PROXY_HOST -Dproxy.https.port=$PROXY_PORT -Dproxy.https.exclude=$PROXY_NONPROXYHOST "$@"
+java -cp "target/lib/*":target/test_fps-0.0.6-SNAPSHOT.jar com.bosa.testfps.Main "$@"
