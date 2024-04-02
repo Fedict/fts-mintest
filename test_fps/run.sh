c06d7bfa5e34a31a708e9b8ce0e6f92267e88fe5
chmo <chmo>
2024-04-02 17:53:41 +0200

    Add https proxy

--- test_fps/run.sh
+++ test_fps/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp "target/lib/*":target/test_fps-0.0.6-SNAPSHOT.jar com.bosa.testfps.Main "$@"
+java -cp "target/lib/*":target/test_fps-0.0.6-SNAPSHOT.jar com.bosa.testfps.Main -Dhttps.proxyHost=dc-proxy.names.belgium.be -Dhttps.proxyPort=3128 -Dhttps.proxyUser=iaa-fts-pr -Dhttps.proxyPassword=CSO4qoHdtY2wPr3E "$@"
