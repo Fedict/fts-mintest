abb80abde8e4c43ff30a7eb9311acca7000163cf
chmo <chmo>
2026-01-12 14:57:09 +0100

    Add keypair double check

--- test_fps/src/main/java/com/bosa/testfps/ECJWTSignerFromPem.java
+++ test_fps/src/main/java/com/bosa/testfps/ECJWTSignerFromPem.java
@@ -4,6 +4,7 @@ import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JWSObject;
 import com.nimbusds.jose.crypto.ECDSASigner;
 import org.bouncycastle.asn1.pkcs.PrivateKeyInfo;
+import org.bouncycastle.jce.interfaces.ECPublicKey;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openssl.PEMKeyPair;
 import org.bouncycastle.openssl.PEMParser;
@@ -11,19 +12,40 @@ import org.bouncycastle.openssl.jcajce.JcaPEMKeyConverter;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.interfaces.ECPrivateKey;
+
 
 public class ECJWTSignerFromPem extends JWTSigner {
     final ECPrivateKey privateKey;
 
-    ECJWTSignerFromPem(String pem) throws IOException, NoSuchAlgorithmException {
+    ECJWTSignerFromPem(String pem) throws IOException, NoSuchAlgorithmException, InvalidKeyException, SignatureException {
         Security.addProvider(new BouncyCastleProvider());
         Object parsed = new PEMParser(new StringReader(pem)).readObject();
         if (parsed instanceof PEMKeyPair) {
             privateKey = (ECPrivateKey) new JcaPEMKeyConverter().getPrivateKey(((PEMKeyPair) parsed).getPrivateKeyInfo());
+
+            testKeypair(privateKey, (ECPublicKey) new JcaPEMKeyConverter().getPublicKey(((PEMKeyPair) parsed).getPublicKeyInfo()));
+
         } else if (parsed instanceof PrivateKeyInfo) privateKey = (ECPrivateKey) new JcaPEMKeyConverter().getPrivateKey((PrivateKeyInfo)parsed);
         else privateKey = null;
+    }
+
+    private void testKeypair(ECPrivateKey privateKey, ECPublicKey publicKey) throws NoSuchAlgorithmException, InvalidKeyException, SignatureException {
+        Signature ecdsaSign = Signature.getInstance("SHA256withECDSA");
+        ecdsaSign.initSign(privateKey);
+        byte[] data = "Message à signer".getBytes(StandardCharsets.UTF_8);
+        ecdsaSign.update(data);
+
+        byte[] signature = ecdsaSign.sign();
+
+        Signature ecdsaVerify = Signature.getInstance("SHA256withECDSA");
+        ecdsaVerify.initVerify(publicKey);
+        ecdsaVerify.update(data);
+        boolean isValid = ecdsaVerify.verify(signature);
+
+        if (!isValid) throw new SignatureException("Signature verification failed");
     }
 
     @Override
@@ -31,3 +53,4 @@ public class ECJWTSignerFromPem extends JWTSigner {
         jwsObject.sign(new ECDSASigner(privateKey));
     }
 }
+;
